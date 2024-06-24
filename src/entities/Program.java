b3fb193cc24dd8cf5f90ea182b0954e92c31d831
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-06-23 21:36:32 -0300

    Algoritmo para calcular a area e o valor de um terreno

--- src/entities/Program.java
+++ src/entities/Program.java
@@ -1,5 +1,8 @@
 package entities;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
@@ -8,8 +11,45 @@ Fazer um programa para ler as medidas da largura e comprimento de um terreno ret
 casa decimal, bem como o valor do metro quadrado do terreno com duas casas decimais. Em seguida,
 o programa deve mostrar o valor da área do terreno, bem como o valor do preço do terreno, ambos com
 duas casas decimais, conforme exemplo. 
-		 */
+		
+Exemplo 1:
+Digite a largura do terreno: 10.0
+Digite o comprimento do terreno: 30.0
+Digite o valor do metro quadrado: 200.00
+Area do terreno = 300.00
+Preco do terreno = 60000.00
 
+Exemplo 2:
+Digite a largura do terreno: 12.0
+Digite o comprimento do terrano: 20.0
+Digite o valor do metro quadrado: 150.00
+Area do terreno = 240.00
+Preco do terreno = 36000.00
+		 
+		 */
+		Locale.setDefault(Locale.US);
+		Scanner leia = new Scanner(System.in);
+		
+		Double largura;
+		double comprimento;
+		double valorMetroTerreno;
+		double areaTerreno;
+		double valorTerreno;
+		
+		System.out.print("Digite a largura do terreno: ");
+		largura = leia.nextDouble();
+		System.out.print("Digite o comprimento do terreno: ");
+		comprimento = leia.nextDouble();
+		areaTerreno = largura * comprimento;
+		System.out.print("Digite o valor do metro quadrado: ");
+		valorMetroTerreno = leia.nextDouble();
+		valorTerreno = areaTerreno * valorMetroTerreno;
+		
+		System.out.printf("Área do terreno = %.2f\n", areaTerreno);
+		System.out.printf("Preço do terreno = %.2f\n", valorTerreno);
+		
+		
+		leia.close();
 	}
 
 }
